e37a4b137d1072621beba50c02d54b38d3f35d9c
Scott D'Alessandro <sdaless22@gmail.com>
2016-01-11 10:36:01 -0500

    Update sometimes.js

--- FoundationsReview/01_sometimes/sometimes.js
+++ FoundationsReview/01_sometimes/sometimes.js
@@ -1,7 +1,7 @@
 function sometimes(fn) {
   var i = 0
   return function() {
-    if (++i >= 4 && (i & 1) === 0) {
+    if (++i >= 4 && ( i%2 === 0)) {
       return "I do not know!"
     }
     return fn.apply(null, arguments)
